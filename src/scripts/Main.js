e90a8b028877cc348b56902d28187832a4cf7837
Nero178 <alexandre.cortes.2001@gmail.com>
2022-03-01 11:19:27 -0500

    avancement etape 1 et 2

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,1 +1,34 @@
 // Vous êtes ici
+class Main {
+    constructor() {
+        // votre code ici
+        this.init();
+        const components = document.querySelectorAll('[data-component="Army"]');
+        this.element.dataset.Army
+    }
+
+    init() {
+        // une deuxième méthode
+    }
+
+}
+new class Army
+{
+    constructor()
+    {
+        console.log('#1 - new Army')
+    }
+
+    init()
+    {
+        const persos = document.querySelectorAll('[data-army="perso"]');
+    }
+}
+
+new class Perso
+{
+    constructor()
+    {
+        
+    }
+}
